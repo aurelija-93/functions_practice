806f561a34aea77a6503408e3eede26db1af6aa8
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-06-03 16:27:04 +0100

    passed 20th test

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -61,3 +61,11 @@ def get_customer_pet_count(customer):
 
 def add_pet_to_customer(customer, pet):
     customer["pets"].append(pet)
+
+# --- OPTIONAL ---
+
+def customer_can_afford_pet(customer, pet):
+    if customer["cash"] >= pet["price"]:
+        return True
+    else:
+        return False
