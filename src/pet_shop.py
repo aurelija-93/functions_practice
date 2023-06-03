68c8583fddf1f77f708a01e22f695715e098d249
Aurelija Bellerby <aurelija.bellerby@gmail.com>
2023-06-03 12:18:51 +0100

    passed 4th test

--- src/pet_shop.py
+++ src/pet_shop.py
@@ -4,3 +4,7 @@ def get_pet_shop_name(pet_shop):
 
 def get_total_cash(pet_shop):
     return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash):
+    pet_shop["admin"]["total_cash"] += cash
+    return get_total_cash(pet_shop)
